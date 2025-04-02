08e6f22f2a179c6840905e85e8e665df25460386
Philippe Côté <cote.philippe@hotmail.com>
2025-04-02 00:24:07 -0400

    Added test setup

---
+++ BingoBackend/tests/BingoBackend.TestUtils/TestSetup.cs
@@ -0,0 +1,54 @@
+using BingoBackend.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BingoBackend.TestUtils;
+
+public static class TestSetup
+{
+    private static IConfigurationRoot GetTestConfiguration()
+    {
+        return new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Test.json")
+            .Build();
+    }
+
+    public static ApplicationDbContext GetApplicationDbContext()
+    {
+        var connectionString = GetTestConfiguration().GetValue<string>("Sqlite:ConnectionString");
+        var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlite(connectionString)
+            .Options;
+        return new ApplicationDbContext(dbContextOptions);
+    }
+
+    public static void RecreateDatabase()
+    {
+        var dbContext = GetApplicationDbContext();
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.Migrate();
+    }
+
+    public static IWebHost BuildWebHost()
+    {
+        return WebHost.CreateDefaultBuilder([])
+            .UseEnvironment("Test")
+            .UseStartup<Startup>()
+            .UseUrls("http://127.0.0.1:0")
+            .Build();
+    }
+
+    public static Uri GetRequiredHostUri(IWebHost webHost)
+    {
+        return new Uri(webHost.Services
+            .GetRequiredService<IServer>()
+            .Features.Get<IServerAddressesFeature>()!
+            .Addresses.First()
+        );
+    }
+}
--- BingoBackend/tests/BingoBackend.Web.Tests/TeamControllerFeatureTest.cs
+++ BingoBackend/tests/BingoBackend.Web.Tests/TeamControllerFeatureTest.cs
@@ -1,13 +1,7 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
-using BingoBackend.Data;
 using BingoBackend.TestUtils;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Hosting.Server.Features;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace BingoBackend.Web.Tests;
 
@@ -16,38 +10,24 @@ public class Tests
     private Uri _baseUrl;
     private IWebHost _host;
 
+    [OneTimeSetUp]
+    public void BeforeAll()
+    {
+        _host = TestSetup.BuildWebHost();
+        _host.Start();
+        _baseUrl = TestSetup.GetRequiredHostUri(_host);
+    }
+
+    [OneTimeTearDown]
+    public void AfterAll()
+    {
+        _host.Dispose();
+    }
+
     [SetUp]
     public void BeforeEach()
     {
-        var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseSqlite(@"Data Source=..\..\..\..\..\data\testdb.db;")
-            .Options;
-        var dbContext = new ApplicationDbContext(dbContextOptions);
-        dbContext.Database.EnsureDeleted();
-        dbContext.Database.Migrate();
-
-        _host = WebHost
-            .CreateDefaultBuilder([])
-            // .ConfigureAppConfiguration(x => x.AddInMemoryCollection(
-            //     new Dictionary<string, string?> { ["Sqlite:ConnectionString"] = "Data Source=:memory:" }
-            // ))
-            .UseEnvironment("Test")
-            .UseStartup<Startup>()
-            .UseUrls("http://127.0.0.1:0")
-            .Build();
-        _host.Start();
-        _baseUrl = new Uri(
-            _host.Services
-                .GetRequiredService<IServer>()
-                .Features.Get<IServerAddressesFeature>()!
-                .Addresses.First()
-        );
-    }
-
-    [TearDown]
-    public void AfterEach()
-    {
-        _host.Dispose();
+        TestSetup.RecreateDatabase();
     }
 
     [Test]
@@ -56,40 +36,20 @@ public class Tests
         var client = new HttpClient();
         var teamName = StringGenerator.GenerateRandomLength(10, 20);
         var jsonContent = /* language=json */
-            $$"""
-              {
-                "name": "{{teamName}}"
-              }
-              """;
-        var stringContent = new StringContent(jsonContent)
-        {
-            Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
-        };
+            $$"""{ "name": "{{teamName}}" }""";
+        var stringContent = new StringContent(jsonContent, new MediaTypeHeaderValue("application/json"));
         var expectedContent = /* language=json */
-            $$"""
-              {
-                "id": {
-                  "__match": {
-                    "type": "integer"
-                  }
-                },
-                "name": "{{teamName}}"
-              }
-              """;
+            $$"""{ "id": { "__match": { "type": "integer" } }, "name": "{{teamName}}" }""";
 
         var response = await client.PostAsync(new Uri(_baseUrl, "/api/teams"), stringContent);
 
         await Expect.StatusCodeFromResponse(HttpStatusCode.Created, response);
+        var responseContent = await response.Content.ReadAsStringAsync();
+        Expect.EquivalentJsonWithPrettyOutput(responseContent, expectedContent);
 
-        var contentText = await response.Content.ReadAsStringAsync();
-        Expect.EquivalentJsonWithPrettyOutput(contentText, expectedContent);
-
-        var response2 = await client.GetAsync(new Uri(_baseUrl, "/api/teams"));
-
-        await Expect.StatusCodeFromResponse(HttpStatusCode.OK, response2);
-        var contentText2 = await response2.Content.ReadAsStringAsync();
-        Console.WriteLine("The value returned:");
-        Console.WriteLine(contentText2);
-        Expect.EquivalentJsonWithPrettyOutput(contentText2, $"[{expectedContent}]");
+        var dbContext = TestSetup.GetApplicationDbContext();
+        var persistedTeams = dbContext.Teams.ToList();
+        Assert.That(persistedTeams, Has.Count.EqualTo(1));
+        Assert.That(persistedTeams[0].Name, Is.EqualTo(teamName));
     }
 }
